2f56349581aff9b57d3bbe3fb40aa089db90410b
Murilo Gonçalves <murillo_goncallves@hotmail.com>
2019-05-09 12:31:42 -0300

    medotos de lista

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
 
 require_once("config.php");
+//carrega 1 usuario
+//$root = new Usuario();
+//$root->loadById(1);
+//echo $root;
 
-$root = new Usuario();
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista)
 
-$root->loadById(1);
+//Carrega uma lista de ususarios buscando pelo Login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+//Carrega um usuario usando login e senha
 
-echo $root;
+$ususario = new Usuario();
+$ususario->login("user","123");
+
+echo $ususario;
 
 ?>
